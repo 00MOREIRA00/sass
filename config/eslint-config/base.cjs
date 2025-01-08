66aa45d609b34545a56798601c7f0053079c7b22
Turbobot <turbobot@vercel.com>
2025-01-08 11:37:25 -0300

    feat(create-turbo): apply official-starter transform

--- config/eslint-config/base.cjs
+++
@@ -1,8 +0,0 @@
-/**@type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['@rocketseat/eslint-config/react', 'plugin:prettier/recommended'],
-  plugins: ['simple-import-sort'],
-  rules: {
-    'simple-import-sort/imports': 'error',
-  },
-}
