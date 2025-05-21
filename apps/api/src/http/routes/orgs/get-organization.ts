aabf84c7637beec134badf5d5e76462ece98ae4a
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2025-05-20 23:51:03 -0300

    feat: adicionando rota de obter uma organizacao e rota de obter todas as organizacoes

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,51 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { BadRequestError } from '../_erros/bad-request-error'
+import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils/create-slug'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organization/:slug',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get details from organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+                organization: z.object({
+                    name: z.string(),
+                    id: z.string().uuid(),
+                    slug: z.string(),
+                    domain: z.string().nullable(),
+                    shouldAttachUsersByDomain: z.boolean(),
+                    avatarUrl: z.string().nullable(),
+                    createdAt: z.date(),
+                    updatedAt: z.date(),
+                    ownerId: z.string().uuid()
+                })
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params;
+        const { organization } = await request.getUserMemberShip(slug);
+
+        return {
+            organization,
+        }
+    
+    },
+    )
+}
